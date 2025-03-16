5355561089f790c877bd71426b0076f3a26ac511
Anurag-amrev7557 <mindreaders7557@gmail.com>
2025-03-16 20:14:45 +0530

    changes

--- frontend/src/pages/login/login.jsx
+++ frontend/src/pages/login/login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './Login.css';
+import './login.css';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
